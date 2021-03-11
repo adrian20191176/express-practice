2f475497150e7f1ef7466ccfd30d0a03644fb9e7
unknown <adrian.20191176@iit.ac.lk>
2021-03-11 19:44:17 +0530

    Html file

---
+++ server copy.js
@@ -0,0 +1,37 @@
+const fs = require('fs');
+const express = require('express');
+
+const app = express();
+
+app.get('/hi',hi)
+app.get('/json',j)
+app.get('/echo',respondEcho)
+app.get('/static/*',respondStatic)
+
+function hi(req,res){
+    res.end("hi");
+}
+
+function j(req,res){
+    res.json({nam:'hi',age:8});
+}
+
+function respondEcho(req,res){
+    const {input = ''} = req.query;
+    res.json({
+        normal:input,
+        shouty:input.toUpperCase(),
+        count:input.length,
+    })
+}
+
+function respondStatic(req,res){
+    const fileName = `${__dirname}/public/${req.params[0]}`
+    fs.createReadStream(fileName)
+        .on('error',() => respondNotFound(req,res))
+        .pipe(res)
+}
+
+const port = process.env.PORT || 1337;
+
+app.listen(port, ()=> console.log(`listinenin ${port}`));
--- server.js
+++ server.js
@@ -1,13 +1,41 @@
+const fs  = require('fs');
 const express = require('express');
+const EventEmitter = require('events');
+
+const chatEmitter = new EventEmitter();
+const port = process.env.PORT || 1337;
 
 const app = express();
 
-app.get('/hi',hi)
+app.get('/chat',respondChat);
+app.get('/sse',respondSSE);
+app.get('/static/*',respondStatic);
 
-function hi(req,res){
-    res.end("hi");
+app.listen(port,() => console.log(`Port Listenin at ${port}`) );
+
+function respondStatic(req,res){
+    const fileName = `${__dirname}/public/${req.params[0]}`;
+    fs.createReadStream(fileName)
+        .on('error', () =>{})
+        .pipe(res)
 }
 
-const port = process.env.PORT || 1337;
+function respondChat(req,res){
+    const {message} = req.query;
+    chatEmitter.emit('message',message)
+    res.end()
+}
 
-app.listen(port, ()=> console.log(`listinenin ${port}`));
+function respondSSE(req,res){
+    res.writeHead(200,{
+        'Content-Type':'text/event-stream',
+        'Connection' : 'keep-alive'
+    })
+
+    const onMessage = msg => res.write(`data: ${msg}\n\n`)
+    chatEmitter.on('message',onMessage)
+
+    res.on('close', function() {
+        chatEmitter.off('message',onMessage)
+    })
+}
